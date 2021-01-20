ead3a95dd469a5261ff1b87b4c3705417ca788b7
zl1509015035 <1509015035@qq.com>
2021-01-20 09:55:20 +0800

    反测试

--- src/zhul/demo01/ThreadCallable.java
+++ src/zhul/demo01/ThreadCallable.java
@@ -4,9 +4,6 @@ import java.util.concurrent.Callable;
 
 public class ThreadCallable implements Callable<Boolean> {
 
-
-
-
     @Override
     public Boolean call() throws Exception {
         return null;
