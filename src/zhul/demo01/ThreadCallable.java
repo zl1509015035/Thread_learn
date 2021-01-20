72247079174f4e061e024498a8c0a2dd346cb390
zl1509015035 <1509015035@qq.com>
2021-01-20 09:54:20 +0800

    创建线程测试

--- src/zhul/demo01/ThreadCallable.java
+++ src/zhul/demo01/ThreadCallable.java
@@ -5,6 +5,8 @@ import java.util.concurrent.Callable;
 public class ThreadCallable implements Callable<Boolean> {
 
 
+
+
     @Override
     public Boolean call() throws Exception {
         return null;
