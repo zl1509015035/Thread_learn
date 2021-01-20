c16c69461542456c13c1bf7292bc48089ab8ec71
zl1509015035 <1509015035@qq.com>
2021-01-20 09:50:49 +0800

    创建线程1

--- src/zhul/demo01/ThreadCallable.java
+++ src/zhul/demo01/ThreadCallable.java
@@ -3,6 +3,8 @@ package zhul.demo01;
 import java.util.concurrent.Callable;
 
 public class ThreadCallable implements Callable<Boolean> {
+
+
     @Override
     public Boolean call() throws Exception {
         return null;
